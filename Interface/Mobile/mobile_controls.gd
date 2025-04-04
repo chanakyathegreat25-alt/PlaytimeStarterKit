b4f594ea375882dd9365ad39e2a1af6888275702
DetectivePikaC2 <166664745+DetectivePikaC2@users.noreply.github.com>
2025-04-04 16:18:57 -0600

    asd

--- Interface/Mobile/mobile_controls.gd
+++ Interface/Mobile/mobile_controls.gd
@@ -1,4 +1,4 @@
 extends Control
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED: Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
