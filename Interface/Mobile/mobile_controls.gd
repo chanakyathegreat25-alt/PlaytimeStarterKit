dac53d027b78cbd3a3fb934f660752fb484c039f
DetectivePikaC2 <166664745+DetectivePikaC2@users.noreply.github.com>
2025-04-09 07:42:47 -0600

    MobileComplete

--- Interface/Mobile/mobile_controls.gd
+++ Interface/Mobile/mobile_controls.gd
@@ -1,7 +1,17 @@
 extends Control
 
 @onready var drag: Control = $Drag
+@onready var gp_extra: Node2D = $GP_Extra
+@onready var watch: TouchScreenButton = $GP_Extra/Watch
 
 func _process(_delta: float) -> void:
 	if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED: Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 	Input.action_press("sprint")
+	if Grabpack.grabpack.current_grabpack > 1:
+		gp_extra.visible = true
+		if Grabpack.grabpack.current_grabpack == 3:
+			watch.visible = true
+		else:
+			watch.visible = false
+	else:
+		gp_extra.visible = false
