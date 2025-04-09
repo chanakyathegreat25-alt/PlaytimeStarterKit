fe98ea92da88f0dd77a4f9290999b0ae4ad1a78b
DetectivePikaC2 <166664745+DetectivePikaC2@users.noreply.github.com>
2025-04-09 16:53:19 -0600

    MobilePatch

--- Interface/Mobile/mobile_controls.gd
+++ Interface/Mobile/mobile_controls.gd
@@ -4,6 +4,28 @@ extends Control
 @onready var gp_extra: Node2D = $GP_Extra
 @onready var watch: TouchScreenButton = $GP_Extra/Watch
 
+func _enter_tree() -> void:
+	disable_input("handleft", MOUSE_BUTTON_LEFT)
+	disable_input("handright", MOUSE_BUTTON_RIGHT)
+func _exit_tree() -> void:
+	enable_input("handleft", MOUSE_BUTTON_LEFT)
+	enable_input("handright", MOUSE_BUTTON_RIGHT)
+
+func disable_input(action_name, button):
+	var button_to_remove = button  # Left mouse button (value = 1)
+
+	# Loop through all input events associated with the action
+	for event in InputMap.action_get_events(action_name):
+		if event is InputEventMouseButton and event.button_index == button_to_remove:
+			InputMap.action_erase_event(action_name, event)
+
+func enable_input(action_name, button):
+	var mouse_event := InputEventMouseButton.new()
+	mouse_event.button_index = button
+	mouse_event.pressed = true  # Required for the input to be considered valid
+
+	InputMap.action_add_event(action_name, mouse_event)
+
 func _process(_delta: float) -> void:
 	if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED: Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 	Input.action_press("sprint")
